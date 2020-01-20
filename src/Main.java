3e22b63dd5963f854a0d63a1821b74ed36c5f969
Simka-for <58855519+simka-for@users.noreply.github.com>
2020-01-20 21:26:20 +0300

    airport
    Stream сортировка

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
-import com.skillbox.airport.Aircraft;
 import com.skillbox.airport.Airport;
+import com.skillbox.airport.Flight;
 
-public class Main
-{
+import java.util.Comparator;
+import java.util.Date;
+
+public class Main {
     public static void main(String[] args) {
+        Airport airport = Airport.getInstance();
+
+        Date date = new Date();
+        date.setHours(date.getHours() + 2);
+        Date now = new Date();
+
+        airport.getTerminals().stream().flatMap(f -> f.getFlights().stream()).
+                filter(flight -> flight.getDate().after(now) && flight.getDate().before(date)).
+                filter(flight -> flight.getType().equals(Flight.Type.DEPARTURE)).
+                sorted(Comparator.comparing(Flight::getDate)).
+                forEach(flight -> System.out.println(flight.getAircraft().getModel() + "\t" + flight));
+
 
     }
 }
